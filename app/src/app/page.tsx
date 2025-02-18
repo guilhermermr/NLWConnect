c8aa4968d8581ab7c5cc663c44bc51513109cbb6
Guilherme Moura <guilhermermribeiro@gmail.com>
2025-02-18 20:08:50 -0300

    Configurancao fonts google

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
     <main>
       <div className="text-white">Hello world!</div>
 
+      <h1 className="text-4xl font-semibold font-heading">Hello World</h1>
+
       <Button text="Guilherme" />
       <Button text="NLW" />
       <Button text="Oi"/>
